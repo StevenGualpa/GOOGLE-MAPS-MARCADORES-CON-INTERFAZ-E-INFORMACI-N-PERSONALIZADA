56730a9dba386babf05efd96fb2b4a17fd1f49c0
Steven Gualpa <stevengualpa@gmail.com>
2022-08-19 10:50:30 -0500

    Version 02
    Lo visto en Clases

--- app/src/main/java/com/example/googlemaps_marcadoresconinterfazeinformacinpersonalizada/MainActivity.kt
+++ app/src/main/java/com/example/googlemaps_marcadoresconinterfazeinformacinpersonalizada/MainActivity.kt
@@ -1,7 +1,15 @@
 package com.example.googlemaps_marcadoresconinterfazeinformacinpersonalizada
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.LocaleList
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -9,9 +17,10 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener , GoogleMap.OnMapClickListener{
 
     private lateinit var map:GoogleMap
+    var REQUEST_CODE_LOCATION=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -34,5 +43,90 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
         CreaMarcador()
+        map.setOnMyLocationButtonClickListener(this)
+        map.setOnMyLocationClickListener(this)
+        map.setOnMapClickListener(this)
+        enableLocation()
     }
+
+
+
+
+    fun MensajeLargo(Mensaje: String)
+    {
+        Toast.makeText(this, Mensaje.toString(), Toast.LENGTH_LONG).show()
+
+    }
+
+    //Gestion de Permisos
+
+    private fun isLocatedPermissionGranted() = ContextCompat.checkSelfPermission(
+        this,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )==PackageManager.PERMISSION_GRANTED
+
+
+
+    private fun enableLocation(){
+        if(!::map.isInitialized)return
+        if(isLocatedPermissionGranted()){
+            map.isMyLocationEnabled=true
+        }else{
+            requesLocationPermission() //Pide el Permiso
+        }
+
+    }
+    private fun requesLocationPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+        MensajeLargo("Acepta los Permisos")
+        }
+        else{
+            ActivityCompat.requestPermissions(  this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),REQUEST_CODE_LOCATION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+           REQUEST_CODE_LOCATION->if(grantResults.isNotEmpty()&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+               map.isMyLocationEnabled=true
+           }else{
+               MensajeLargo("Para Actucar la localizacion ve a ajustes y acepta los permisos")
+           }
+           else->{}
+
+       }
+    }
+
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        if (!::map.isInitialized) return
+        if (!isLocatedPermissionGranted()) {
+            map.isMyLocationEnabled = false
+            MensajeLargo("Para Activar la localizacion acepta los permisos")
+        }
+    }
+
+
+    override fun onMyLocationButtonClick(): Boolean {
+        MensajeLargo("Boton Pulsado")
+        return true   //False A mi Ubicacion true no hace nada
+    }
+
+    override fun onMyLocationClick(p0: Location) {
+            MensajeLargo("Estas en ${p0.latitude},${p0.longitude}")
+    }
+
+    override fun onMapClick(p0: LatLng) {
+        MensajeLargo("Estas en ${p0.latitude},${p0.longitude}")
+
+    }
+
+
 }
