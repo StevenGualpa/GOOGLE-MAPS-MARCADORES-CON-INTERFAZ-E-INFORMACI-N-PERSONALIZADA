83002e73f921f65be56e8fd231e2e9d2fd6665a5
Steven Gualpa <stevengualpa@gmail.com>
2022-08-19 01:50:12 -0500

    Version 01
    Mapa Sencillo

--- app/src/main/java/com/example/googlemaps_marcadoresconinterfazeinformacinpersonalizada/MainActivity.kt
+++ app/src/main/java/com/example/googlemaps_marcadoresconinterfazeinformacinpersonalizada/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.googlemaps_marcadoresconinterfazeinformacinpersonalizada
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var map:GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        MuestraMapa()
+    }
+
+    private fun MuestraMapa(){
+        val mapFragment:SupportMapFragment=supportFragmentManager.findFragmentById(R.id.fragmentMap) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+    //Funcion Para Agregar Marcador
+    private fun CreaMarcador(){
+        val coordenadas=LatLng(-1.0803351324691082, -79.50145350501472)
+        val marcador: MarkerOptions =MarkerOptions().position(coordenadas).title("UTEQ")
+        map.addMarker(marcador)
+        //Animacion para Dirigir la camara hacia el marcador
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(coordenadas,18f),5000,null)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        CreaMarcador()
     }
 }
